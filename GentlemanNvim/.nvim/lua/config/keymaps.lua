45320c319da3112bcd3ca1a2c3bb831b2072fbb3
Alan Buscaglia <alanbuscaglia@gmail.com>
2023-12-12 17:23:36 +0100

    Update keymaps.lua

--- GentlemanNvim/.nvim/lua/config/keymaps.lua
+++ GentlemanNvim/.nvim/lua/config/keymaps.lua
@@ -1,6 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua Add any additional keymaps here
 
+local nvim_tmux_nav = require("nvim-tmux-navigation")
+
 vim.keymap.set("i", "<C-d>", "<C-d>zz")
 vim.keymap.set("i", "<C-u>", "<C-u>zz")
 vim.keymap.set("i", "<C-b>", "<C-o>de")
+vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
