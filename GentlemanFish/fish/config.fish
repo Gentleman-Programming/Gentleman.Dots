c8acea6ac6efbbf4acfe5e7d871153b9bdde5edf
alan buscaglia <alanbuscaglia@gmail.com>
2024-02-26 13:50:21 +0100

    feature: added zellij to fish config

--- GentlemanFish/fish/config.fish
+++ GentlemanFish/fish/config.fish
@@ -4,3 +4,46 @@ end
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+#
+# if status is-interactive
+#     and not set -q TMUX
+#     exec tmux
+# end
+
+if set -q ZELLIJ
+else
+    zellij
+end
+
+set -l foreground DCD7BA
+set -l selection 2D4F67
+set -l comment 727169
+set -l red C34043
+set -l orange FF9E64
+set -l yellow C0A36E
+set -l green 76946A
+set -l purple 957FB8
+set -l cyan 7AA89F
+set -l pink D27E99
+
+# Syntax Highlighting Colors
+set -g fish_color_normal $foreground
+set -g fish_color_command $cyan
+set -g fish_color_keyword $pink
+set -g fish_color_quote $yellow
+set -g fish_color_redirection $foreground
+set -g fish_color_end $orange
+set -g fish_color_error $red
+set -g fish_color_param $purple
+set -g fish_color_comment $comment
+set -g fish_color_selection --background=$selection
+set -g fish_color_search_match --background=$selection
+set -g fish_color_operator $green
+set -g fish_color_escape $pink
+set -g fish_color_autosuggestion $comment
+
+# Completion Pager Colors
+set -g fish_pager_color_progress $comment
+set -g fish_pager_color_prefix $cyan
+set -g fish_pager_color_completion $foreground
+set -g fish_pager_color_description $comment
