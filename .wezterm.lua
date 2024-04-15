2f672e0de7f73691a437dbb0ad966aed98104281
Alan Buscaglia <alanbuscaglia@gmail.com>
2024-04-15 12:13:17 +0200

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,27 @@
 local wezterm = require 'wezterm'
 
 -- This table will hold the configuration.
-local config = {}
+local config = {
+	force_reverse_video_cursor = true,
+	colors = {
+		foreground = "#dcd7ba",
+		background = "#181616",
+
+		cursor_bg = "#c8c093",
+		cursor_fg = "#c8c093",
+		cursor_border = "#c8c093",
+
+		selection_fg = "#c8c093",
+		selection_bg = "#2d4f67",
+
+		scrollbar_thumb = "#16161d",
+		split = "#16161d",
+
+		ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+		brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+		indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+	},
+}
 
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
@@ -10,42 +30,33 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- Kanagawa colors with dragon background
-config.color_scheme = 'Kanagawa (Gogh)'
-config.window_background_gradient = {
-  colors = {'#181616'}
-}
-
--- Set opacity, you may need "WebGpu" as your front_end
+-- This is where you actually apply your config choices
+config.force_reverse_video_cursor = true
+config.colors = {}
+config.colors.foreground = "#dcd7ba"
+config.colors.background = "#181616"
+config.colors.cursor_bg = "#c8c093"
+config.colors.cursor_fg = "#c8c093"
+config.colors.cursor_border = "#c8c093"
+config.colors.selection_fg = "#c8c093"
+config.colors.selection_bg = "#2d4f67"
+config.colors.scrollbar_thumb = "#16161d"
+config.colors.split = "#16161d"
+config.colors.ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" }
+config.colors.brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" }
+config.colors.indexed = { [16] = "#ffa066", [17] = "#ff5d62" }
+--  change
 config.window_background_opacity = 0.95
 config.font = wezterm.font 'IosevkaTerm NFM'
-
--- Only show one tab if needed --
-
-config.hide_tab_bar_if_only_one_tab = true
-
--- For Linux / OSX (uncomment if needed)
-
--- config.default_prog = { '/home/linuxbrew/.linuxbrew/bin/fish', '-l' }
-
--- For windows -- (comment if you want to use Linux / OSX)
-
--- Start directly in WSL
 config.default_domain = 'WSL:Ubuntu'
-
--- Full usage of GPU for rendering
 config.front_end = "WebGpu"
-
--- Change fps to your monitor ones --
+config.hide_tab_bar_if_only_one_tab = true
 config.max_fps = 120
-
--- Fix for nvidia, opacity not working
 for _, gpu in ipairs(wezterm.gui.enumerate_gpus()) do
 	if gpu.backend == "Vulkan" then
 		config.webgpu_preferred_adapter = gpu
 		break
 	end
 end
-
 -- and finally, return the configuration to wezterm
 return config
