148b40fed68121a3ed5c6d80c5fef7b29d95f7ba
alan buscaglia <alanbuscaglia@gmail.com>
2024-06-06 15:21:57 +0200

    fix: correct background color

--- GentlemanNvim/nvim/lua/plugins/colorscheme.lua
+++ GentlemanNvim/nvim/lua/plugins/colorscheme.lua
@@ -73,6 +73,12 @@ return {
   },
   {
     "sainnhe/everforest",
+    config = function()
+      vim.g.everforest_transparent_background = 1
+      vim.g.everforest_background = "hard"
+      vim.g.everforest_enable_italic = 1
+      vim.g.everforest_float_style = "dim"
+    end,
   },
   {
     "LazyVim/LazyVim",
