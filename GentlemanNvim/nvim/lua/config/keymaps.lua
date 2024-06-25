15ebbb2e4c08345aa9ead73a0103b5ac62d9a4de
Alan Buscaglia <alan.buscaglia@gft.com>
2024-06-25 16:21:28 +0200

    fix: better escape

--- GentlemanNvim/nvim/lua/config/keymaps.lua
+++ GentlemanNvim/nvim/lua/config/keymaps.lua
@@ -8,8 +8,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- Map Ctrl+b in insert mode to delete to the end of the word without leaving insert mode
 vim.keymap.set("i", "<C-b>", "<C-o>de")
 
--- Map jk to escape from insert mode
-vim.keymap.set("i", "jk", [[<C-\><C-n>]])
+-- Map Ctrl+c to escape from other modes
+vim.keymap.set({ "i", "n", "v" }, "<C-c>", [[<C-\><C-n>]])
 
 ----- Tmux Navigation ------
 local nvim_tmux_nav = require("nvim-tmux-navigation")
