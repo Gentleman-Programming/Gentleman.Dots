61528afc4da5f636cb6f4fafc7c60c118083c6c6
Alan Buscaglia <alanbuscaglia@gmail.com>
2024-07-09 13:01:18 +0200

    Update editor.lua

--- GentlemanNvim/nvim/lua/plugins/editor.lua
+++ GentlemanNvim/nvim/lua/plugins/editor.lua
@@ -2,6 +2,49 @@
 
 return {
   {
+    "rmagatti/goto-preview",
+    event = "BufEnter",
+    config = true, -- necessary as per https://github.com/rmagatti/goto-preview/issues/88
+    keys = {
+      {
+        "gpd",
+        "<cmd>lua require('goto-preview').goto_preview_definition()<CR>",
+        noremap = true,
+        desc = "goto preview definition",
+      },
+      {
+        "gpD",
+        "<cmd>lua require('goto-preview').goto_preview_declaration()<CR>",
+        noremap = true,
+        desc = "goto preview declaration",
+      },
+      {
+        "gpi",
+        "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>",
+        noremap = true,
+        desc = "goto preview implementation",
+      },
+      {
+        "gpy",
+        "<cmd>lua require('goto-preview').goto_preview_type_definition()<CR>",
+        noremap = true,
+        desc = "goto preview type definition",
+      },
+      {
+        "gpr",
+        "<cmd>lua require('goto-preview').goto_preview_references()<CR>",
+        noremap = true,
+        desc = "goto preview references",
+      },
+      {
+        "gP",
+        "<cmd>lua require('goto-preview').close_all_win()<CR>",
+        noremap = true,
+        desc = "close all preview windows",
+      },
+    },
+  },
+  {
     "echasnovski/mini.hipatterns",
     event = "BufReadPre",
     opts = {
