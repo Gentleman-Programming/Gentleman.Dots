3a73b8d82f2794983afa56a341931e26d0a08b9b
Alan Buscaglia <alanbuscaglia@gmail.com>
2024-07-22 15:49:21 +0200

    Update autocmds.lua Not working :(

--- GentlemanNvim/nvim/lua/config/autocmds.lua
+++ GentlemanNvim/nvim/lua/config/autocmds.lua
@@ -1,9 +1,3 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(client, buffer)
-    vim.lsp.inlay_hint.enable(false, { bufnr = buffer })
-  end,
-})
