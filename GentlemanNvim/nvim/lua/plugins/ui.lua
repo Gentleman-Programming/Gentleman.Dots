189a67ee99a90d735229be17059fb93a6dd68266
alan buscaglia <alanbuscaglia@gmail.com>
2024-07-24 00:35:56 +0200

    feature: add icon to lualine

--- GentlemanNvim/nvim/lua/plugins/ui.lua
+++ GentlemanNvim/nvim/lua/plugins/ui.lua
@@ -110,10 +110,20 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
+    requires = { "nvim-tree/nvim-web-devicons", opt = true },
     opts = {
       options = {
         -- globalstatus = false,
         theme = "everforest",
+        icons_enabled = true,
+      },
+      sections = {
+        lualine_a = {
+          {
+            "mode",
+            icon = "󱗞",
+          },
+        },
       },
     },
   },
