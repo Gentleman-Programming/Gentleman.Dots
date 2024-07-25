47fafa32ccf8fbe79f041d349a2425b378e13087
alan buscaglia <alanbuscaglia@gmail.com>
2024-07-25 14:34:10 +0200

    feature: add dap-core to extras

--- GentlemanNvim/nvim/lua/config/lazy.lua
+++ GentlemanNvim/nvim/lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    { import = "lazyvim.plugins.extras.dap.core" },
 
     -- import/override with your plugins
     { import = "plugins" },
