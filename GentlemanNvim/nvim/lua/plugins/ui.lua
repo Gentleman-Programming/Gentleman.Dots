2f92417020108a07d6d1e3e742112f17611eb6b1
Fernando Galindo Martinez <fernando.galindo@gft.com>
2024-08-06 15:05:26 +0200

    feat: remove buffer line code

--- GentlemanNvim/nvim/lua/plugins/ui.lua
+++ GentlemanNvim/nvim/lua/plugins/ui.lua
@@ -79,25 +79,6 @@ return {
     },
   },
 
-  -- buffer line
-  {
-    "akinsho/bufferline.nvim",
-    event = "VeryLazy",
-    enabled = false,
-    keys = {
-      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-    },
-    opts = {
-      options = {
-        mode = "tabs",
-        -- separator_style = "slant",
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-      },
-    },
-  },
-
   -- statusline
   {
     "nvim-lualine/lualine.nvim",
