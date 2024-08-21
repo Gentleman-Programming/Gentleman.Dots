3b49a9a5d9f05350b73539758db685e0a7803955
alan buscaglia <alanbuscaglia@gmail.com>
2024-08-21 16:15:34 +0200

    fix: better markdown preview, fix wsl copy/paste

--- GentlemanNvim/nvim/lua/plugins/disabled.lua
+++ GentlemanNvim/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,10 @@
--- buffer line
 return {
   {
     "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
     enabled = false,
   },
 }
--- GentlemanNvim/nvim/lua/plugins/markdown-preview.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  ft = { "markdown" },
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
-}
