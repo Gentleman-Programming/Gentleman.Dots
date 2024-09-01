911372c08d71e27af7894c9ab7dceb0e6c0f6477
alan buscaglia <alanbuscaglia@gmail.com>
2024-09-01 15:25:01 +0200

    feat: classic cmdline

--- GentlemanNvim/nvim/lua/plugins/ui.lua
+++ GentlemanNvim/nvim/lua/plugins/ui.lua
@@ -1,29 +1,25 @@
 return {
   -- messages, cmdline and the popupmenu
   {
-
     "folke/noice.nvim",
     config = function()
       require("noice").setup({
+        cmdline = {
+          view = "cmdline",
+        },
         presets = {
           bottom_search = true,
           command_palette = true,
           lsp_doc_border = true,
         },
-        views = {
-          cmdline_popup = {
-            border = {
-              style = "none",
-              padding = { 2, 3 },
-            },
-            filter_options = {},
-
-            win_options = {
-
-              winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-            },
-          },
-        },
+        -- views = {
+        --   cmdline_popup = {
+        --     filter_options = {},
+        --     win_options = {
+        --       winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+        --     },
+        --   },
+        -- },
       })
     end,
   },
