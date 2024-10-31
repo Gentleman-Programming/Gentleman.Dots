393659339e41e40fc7f1a47f6a2e78068843206a
alan buscaglia <alanbuscaglia@gmail.com>
2024-10-31 17:41:22 +0100

    feat: implement kanagawa for fish and zellij

--- GentlemanFish/fish/config.fish
+++ GentlemanFish/fish/config.fish
@@ -26,35 +26,36 @@ end
 starship init fish | source
 
 set -x LS_COLORS "di=38;5;73:ow=48;5;22:ex=38;5;131:ln=38;5;179:*.tar=38;5;209:*.zip=38;5;209:*.jpg=38;5;176:*.png=38;5;176:*.mp3=38;5;176:*.wav=38;5;176:*.txt=38;5;223:*.sh=38;5;131"
+set -g fish_greeting ""
 
 ## alias
 alias ls='gls --color=auto'
 alias fzfbat='fzf --preview="bat --theme=gruvbox-dark --color=always {}"'
 alias fzfnvim='nvim (fzf --preview="bat --theme=gruvbox-dark --color=always {}")'
 
-## kanagawa
-#set -l foreground DCD7BA
-#set -l selection 2D4F67
-#set -l comment 727169
-#set -l red C34043
-#set -l orange FF9E64
-#set -l yellow C0A36E
-#set -l green 76946A
-#set -l purple 957FB8
-#set -l cyan 7AA89F
-#set -l pink D27E99
+# kanagawa
+set -l foreground DCD7BA
+set -l selection 2D4F67
+set -l comment 727169
+set -l red C34043
+set -l orange FF9E64
+set -l yellow C0A36E
+set -l green 76946A
+set -l purple 957FB8
+set -l cyan 7AA89F
+set -l pink D27E99
 
 ## everforest
-set -l foreground d3c6aa
-set -l selection 2d4f67
-set -l comment 859289
-set -l red e67e80
-set -l orange ff9e64
-set -l yellow dbbc7f
-set -l green a7c080
-set -l purple d699b6
-set -l cyan 7fbbb3
-set -l pink d699b6
+#set -l foreground d3c6aa
+#set -l selection 2d4f67
+#set -l comment 859289
+#set -l red e67e80
+#set -l orange ff9e64
+#set -l yellow dbbc7f
+#set -l green a7c080
+#set -l purple d699b6
+#set -l cyan 7fbbb3
+#set -l pink d699b6
 
 # Syntax Highlighting Colors
 set -g fish_color_normal $foreground
