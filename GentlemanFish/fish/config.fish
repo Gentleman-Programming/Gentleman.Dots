b1840bedc0744ecc2a88a9685b544820be8326fe
Alan Buscaglia <alanbuscaglia@gmail.com>
2024-11-01 01:40:07 +0100

    Update config.fish

--- GentlemanFish/fish/config.fish
+++ GentlemanFish/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set PROJECT_PATHS /your/work/path/
+set PROJECT_PATHS /home/alanbuscaglia/work
 
 if test (uname) = Darwin
     # macOS
@@ -25,25 +25,13 @@ end
 
 starship init fish | source
 
-set -x LS_COLORS "di=38;5;73:ow=48;5;22:ex=38;5;131:ln=38;5;179:*.tar=38;5;209:*.zip=38;5;209:*.jpg=38;5;176:*.png=38;5;176:*.mp3=38;5;176:*.wav=38;5;176:*.txt=38;5;223:*.sh=38;5;131"
+set -x LS_COLORS "di=38;5;67:ow=48;5;60:ex=38;5;132:ln=38;5;144:*.tar=38;5;180:*.zip=38;5;180:*.jpg=38;5;175:*.png=38;5;175:*.mp3=38;5;175:*.wav=38;5;175:*.txt=38;5;223:*.sh=38;5;132"
 set -g fish_greeting ""
 
 ## alias
 alias ls='gls --color=auto'
 alias fzfbat='fzf --preview="bat --theme=gruvbox-dark --color=always {}"'
 alias fzfnvim='nvim (fzf --preview="bat --theme=gruvbox-dark --color=always {}")'
-
-# kanagawa
-set -l foreground DCD7BA
-set -l selection 2D4F67
-set -l comment 727169
-set -l red C34043
-set -l orange FF9E64
-set -l yellow C0A36E
-set -l green 76946A
-set -l purple 957FB8
-set -l cyan 7AA89F
-set -l pink D27E99
 
 ## everforest
 #set -l foreground d3c6aa
@@ -56,6 +44,18 @@ set -l pink D27E99
 #set -l purple d699b6
 #set -l cyan 7fbbb3
 #set -l pink d699b6
+
+# kanagawa dragon colors
+set -l foreground C5C9C5 # dragonWhite - un blanco suave para texto principal
+set -l selection 2D4F67 # waveBlue2 - azul oscuro para selección
+set -l comment 7A8382 # dragonGray3 - gris para comentarios
+set -l red C4746E # dragonRed - rojo suave
+set -l orange B98D7B # dragonOrange2 - naranja suave
+set -l yellow C4B28A # dragonYellow - amarillo cálido
+set -l green 87A987 # dragonGreen - verde pastel
+set -l purple 957FB8 # oniViolet - púrpura suave
+set -l cyan 8EA4A2 # dragonAqua - verde azulado
+set -l pink D27E99 # sakuraPink - rosa suave
 
 # Syntax Highlighting Colors
 set -g fish_color_normal $foreground
