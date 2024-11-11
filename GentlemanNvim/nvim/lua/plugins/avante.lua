38bcfd874e05d43f21490a51b3611a2886d66da7
alan buscaglia <alanbuscaglia@gmail.com>
2024-11-11 18:49:39 +0100

    feat: avante config

--- GentlemanNvim/nvim/lua/plugins/avante.lua
+++ GentlemanNvim/nvim/lua/plugins/avante.lua
@@ -49,26 +49,24 @@ return {
     hints = { enabled = true },
     windows = {
       ---@type "right" | "left" | "top" | "bottom"
-      position = "right", -- the position of the sidebar
+      position = "left", -- the position of the sidebar
       wrap = true, -- similar to vim.o.wrap
       width = 30, -- default % based on available width
       sidebar_header = {
         enabled = true, -- true, false to enable/disable the header
         align = "center", -- left, center, right for title
-        rounded = true,
+        rounded = false,
       },
       input = {
         prefix = "> ",
         height = 8, -- Height of the input window in vertical layout
       },
       edit = {
-        border = "rounded",
         start_insert = true, -- Start insert mode when opening the edit window
       },
       ask = {
-        floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+        floating = true, -- Open the 'AvanteAsk' prompt in a floating window
         start_insert = true, -- Start insert mode when opening the ask window
-        border = "rounded",
         ---@type "ours" | "theirs"
         focus_on_apply = "ours", -- which diff to focus after applying
       },
