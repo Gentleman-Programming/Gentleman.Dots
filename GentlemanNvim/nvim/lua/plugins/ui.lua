81789c8d03857466a565a848948cacb580d2e2fd
alan buscaglia <alanbuscaglia@gmail.com>
2024-11-19 22:41:17 +0100

    fix: correct sections dashboard

--- GentlemanNvim/nvim/lua/plugins/ui.lua
+++ GentlemanNvim/nvim/lua/plugins/ui.lua
@@ -140,14 +140,14 @@ return {
     "folke/snacks.nvim",
     opts = {
       dashboard = {
+        sections = {
+          { section = "header" },
+          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          { section = "startup" },
+        },
         preset = {
-          sections = {
-            { section = "header" },
-            { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-            { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-            { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-            { section = "startup" },
-          },
           header = [[
                     ░░░░░░      ░░░░░░                        
                   ░░░░░░░░░░  ░░░░░░░░░░                      
