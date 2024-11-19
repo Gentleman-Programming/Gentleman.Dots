bd6bee054262f33dc3aab84f56940751a175ed6b
alan buscaglia <alanbuscaglia@gmail.com>
2024-11-19 17:23:22 +0100

    feat: update to new snack dashboard

--- GentlemanNvim/nvim/lua/plugins/ui.lua
+++ GentlemanNvim/nvim/lua/plugins/ui.lua
@@ -132,30 +132,44 @@ return {
     keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } }, -- Keybinding to toggle Zen Mode
   },
 
-  -- Plugin: dashboard-nvim
-  -- URL: https://github.com/nvimdev/dashboard-nvim
+  -- Plugin: snacks.nvim
+  -- URL: https://github.com/folke/snacks.nvim/tree/main
   -- Description: A Neovim plugin for creating a customizable dashboard.
   {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter", -- Load this plugin on VimEnter event
-    opts = function(_, opts)
-      local logo = [[
-                      ░░░░░░      ░░░░░░                      
-                    ░░░░░░░░░░  ░░░░░░░░░░                    
-                  ░░░░░░░░░░░░░░░░░░░░░░░░░░                  
-                ░░░░░░░░░░▒▒▒▒░░▒▒▒▒░░░░░░░░░░                
-              ░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░              
-  ▒▒        ░░░░░░▒▒▒▒▒▒▒▒▒▒██▒▒██▒▒▒▒▒▒▒▒▒▒░░░░░░        ▒▒  
-▒▒░░    ░░░░░░░░▒▒▒▒▒▒▒▒▒▒████▒▒████▒▒▒▒▒▒▒▒▒▒░░░░░░░░    ░░▒▒
-▒▒▒▒░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒██████▒▒██████▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒
-██▒▒▒▒▒▒▒▒▒▒▒▒▒▒██▒▒▒▒██████▓▓██▒▒██████▒▒▓▓██▒▒▒▒▒▒▒▒▒▒▒▒▒▒██
-  ████▒▒▒▒▒▒████▒▒▒▒██████████  ██████████▒▒▒▒████▒▒▒▒▒▒▒▒██  
-    ████████████████████████      ████████████████████████    
-      ██████████████████              ██████████████████      
-          ██████████                      ██████████          
-]]
-      logo = string.rep("\n", 8) .. logo .. "\n\n" -- Add padding to the logo
-      opts.config.header = vim.split(logo, "\n") -- Set the header for the dashboard
-    end,
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
+                    ░░░░░░      ░░░░░░                        
+                  ░░░░░░░░░░  ░░░░░░░░░░                      
+                ░░░░░░░░░░░░░░░░░░░░░░░░░░                    
+              ░░░░░░░░░░▒▒▒▒░░▒▒▒▒░░░░░░░░░░                  
+  ░░░░      ░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░        ░░░░    
+▒▒░░      ░░░░░░▒▒▒▒▒▒▒▒▒▒██▒▒██▒▒▒▒▒▒▒▒▒▒░░░░░░        ▒▒░░  
+▒▒░░    ░░░░░░░░▒▒▒▒▒▒▒▒▒▒████▒▒████▒▒▒▒▒▒▒▒▒▒░░░░░░░░  ▒▒░░▒ 
+▒▒▒▒░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒██████▒▒██████▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒ 
+██▒▒▒▒▒▒▒▒▒▒▒▒▒▒██▒▒▒▒██████▓▓██▒▒██████▒▒▓▓██▒▒▒▒▒▒▒▒▒▒▒▒▒▒█ 
+████▒▒▒▒▒▒████▒▒▒▒██████████  ██████████▒▒▒▒████▒▒▒▒▒▒▒▒██    
+  ████████████████████████      ████████████████████████      
+    ██████████████████              ██████████████████        
+        ██████████                      ██████████            
+]],
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+        },
+      },
+    },
   },
 }
