181439145668bf65c27e0ae3697930a1f934d4c4
alan buscaglia <alanbuscaglia@gmail.com>
2024-11-26 14:05:43 +0100

    fix: change keymaps so they don't conflict with zellij

--- GentlemanNvim/nvim/lua/plugins/overrides.lua
+++ GentlemanNvim/nvim/lua/plugins/overrides.lua
@@ -41,4 +41,30 @@ return {
       },
     },
   },
+  {
+    -- Plugin: fzf-lua
+    -- URL: https://github.com/ibhagwan/fzf-lua
+    -- Description: A command-line fuzzy finder written in Lua.
+    "ibhagwan/fzf-lua",
+    cmd = "FzfLua",
+    opts = function(_)
+      local actions = require("fzf-lua.actions")
+
+      return {
+        files = {
+          cwd_prompt = false,
+          actions = {
+            ["ctrl-i"] = { actions.toggle_ignore },
+            ["ctrl-h"] = { actions.toggle_hidden },
+          },
+        },
+        grep = {
+          actions = {
+            ["ctrl-i"] = { actions.toggle_ignore },
+            ["ctrl-h"] = { actions.toggle_hidden },
+          },
+        },
+      }
+    end,
+  },
 }
