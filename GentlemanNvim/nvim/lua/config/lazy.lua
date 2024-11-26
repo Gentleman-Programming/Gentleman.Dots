53233675053add95bc2e9f0e159cbc584fc9a215
alan buscaglia <alanbuscaglia@gmail.com>
2024-11-26 13:54:57 +0100

    feat: add fzf-lua to default config

--- GentlemanNvim/nvim/lua/config/lazy.lua
+++ GentlemanNvim/nvim/lua/config/lazy.lua
@@ -38,6 +38,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
+    { import = "lazyvim.plugins.extras.editor.fzf" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.dap.core" },
