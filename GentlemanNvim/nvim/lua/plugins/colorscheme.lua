87442150e0fad94f8d92862d5c74b3429bbe6db3
alan buscaglia <alanbuscaglia@gmail.com>
2024-11-26 13:53:31 +0100

    feat: change telescope for fzf-lua

--- GentlemanNvim/nvim/lua/plugins/colorscheme.lua
+++ GentlemanNvim/nvim/lua/plugins/colorscheme.lua
@@ -1,32 +1,32 @@
 return {
-  {
-    -- Plugin for the Catppuccin color scheme
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false, -- Load this plugin immediately
-    opts = {
-      transparent_background = true, -- Enable transparent background
-      flavour = "mocha", -- Set the flavor of the color scheme
-    },
-    integrations = {
-      cmp = true, -- Enable integration with nvim-cmp
-      gitsigns = true, -- Enable integration with gitsigns
-      nvimtree = true, -- Enable integration with nvim-tree
-      treesitter = true, -- Enable integration with treesitter
-      notify = false, -- Disable integration with nvim-notify
-      mini = {
-        enabled = true, -- Enable mini plugin integration
-        indentscope_color = "", -- Set indentscope color (empty means default)
-      },
-      -- Additional plugin integrations can be found in the documentation
-    },
-  },
-  {
-    -- Plugin for the Modus Themes
-    "miikanissi/modus-themes.nvim",
-    name = "modus",
-    priority = 1000, -- High priority to ensure it loads early
-  },
+  -- {
+  --   -- Plugin for the Catppuccin color scheme
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   lazy = false, -- Load this plugin immediately
+  --   opts = {
+  --     transparent_background = true, -- Enable transparent background
+  --     flavour = "mocha", -- Set the flavor of the color scheme
+  --   },
+  --   integrations = {
+  --     cmp = true, -- Enable integration with nvim-cmp
+  --     gitsigns = true, -- Enable integration with gitsigns
+  --     nvimtree = true, -- Enable integration with nvim-tree
+  --     treesitter = true, -- Enable integration with treesitter
+  --     notify = false, -- Disable integration with nvim-notify
+  --     mini = {
+  --       enabled = true, -- Enable mini plugin integration
+  --       indentscope_color = "", -- Set indentscope color (empty means default)
+  --     },
+  --     -- Additional plugin integrations can be found in the documentation
+  --   },
+  -- },
+  -- {
+  --   -- Plugin for the Modus Themes
+  --   "miikanissi/modus-themes.nvim",
+  --   name = "modus",
+  --   priority = 1000, -- High priority to ensure it loads early
+  -- },
   {
     -- Plugin for the Kanagawa color scheme
     "rebelot/kanagawa.nvim",
@@ -47,13 +47,9 @@ return {
           LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim }, -- Custom colors for Lazy plugin
           MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim }, -- Custom colors for Mason plugin
 
-          TelescopeTitle = { fg = theme.ui.special, bold = true }, -- Custom colors for Telescope titles
-          TelescopePromptNormal = { bg = theme.ui.bg_p1 }, -- Custom colors for Telescope prompt
-          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 }, -- Custom colors for Telescope prompt border
-          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 }, -- Custom colors for Telescope results
-          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 }, -- Custom colors for Telescope results border
-          TelescopePreviewNormal = { bg = theme.ui.bg_dim }, -- Custom colors for Telescope preview
-          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim }, -- Custom colors for Telescope preview border
+          FzfLuaNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 }, -- Custom colors for FzfLua normal
+          FzfLuaBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 }, -- Custom colors for FzfLua border
+          FzfLuaTitle = { fg = theme.ui.special, bold = true }, -- Custom colors for FzfLua title
 
           Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- Custom colors for popup menu
           PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 }, -- Custom colors for selected item in popup menu
@@ -64,54 +60,54 @@ return {
     },
   },
   -- Plugin for the Rose Pine color scheme
-  { "rose-pine/neovim", name = "rose-pine" },
+  -- { "rose-pine/neovim", name = "rose-pine" },
   -- Plugin for the Oxocarbon color scheme
-  { "nyoom-engineering/oxocarbon.nvim", name = "oxocarbon" },
+  -- { "nyoom-engineering/oxocarbon.nvim", name = "oxocarbon" },
   -- Plugin for the Lush color scheme with a dependency on Zenbones
-  { "rktjmp/lush.nvim", dependencies = { "mcchrish/zenbones.nvim" } },
-  {
-    -- Plugin for the Cyberdream color scheme
-    "scottmckendry/cyberdream.nvim",
-    lazy = false, -- Load this plugin immediately
-    priority = 1000, -- High priority to ensure it loads early
-  },
+  -- { "rktjmp/lush.nvim", dependencies = { "mcchrish/zenbones.nvim" } },
+  -- {
+  --   -- Plugin for the Cyberdream color scheme
+  --   "scottmckendry/cyberdream.nvim",
+  --   lazy = false, -- Load this plugin immediately
+  --   priority = 1000, -- High priority to ensure it loads early
+  -- },
   -- Plugin for the Doom One color scheme
-  { "NTBBloodbath/doom-one.nvim", name = "doom-one" },
+  -- { "NTBBloodbath/doom-one.nvim", name = "doom-one" },
   -- Plugin for the Aylin color scheme
-  { "AhmedAbdulrahman/aylin.vim", name = "aylin" },
-  {
-    -- Plugin for enabling transparency
-    "xiyaowong/transparent.nvim",
-  },
-  {
-    -- Plugin for the Bamboo color scheme
-    "ribru17/bamboo.nvim",
-    lazy = false, -- Load this plugin immediately
-  },
-  {
-    -- Plugin for the Everforest color scheme
-    "neanias/everforest-nvim",
-    version = false, -- Use the latest version
-    lazy = false, -- Load this plugin immediately
-    config = function()
-      require("everforest").setup({
-        background = "hard", -- Set the background to 'hard'
-        italis = true, -- Enable italics
-        transparent_background_level = 1, -- Uncomment to enable transparency
-        diagnostic_text_highlight = true, -- Enable diagnostic text highlight
-        diagnostic_virtual_text = "coloured", -- Set diagnostic virtual text to colored
-        colours_override = function(palette)
-          palette.bg0 = "#1A1A22" -- Override background color
-        end,
-      })
-    end,
-  },
-  {
-    -- Plugin for the Oldworld color scheme
-    "dgox16/oldworld.nvim",
-    lazy = false, -- Load this plugin immediately
-    priority = 1000, -- High priority to ensure it loads early
-  },
+  -- { "AhmedAbdulrahman/aylin.vim", name = "aylin" },
+  -- {
+  --   -- Plugin for enabling transparency
+  --   "xiyaowong/transparent.nvim",
+  -- },
+  -- {
+  --   -- Plugin for the Bamboo color scheme
+  --   "ribru17/bamboo.nvim",
+  --   lazy = false, -- Load this plugin immediately
+  -- },
+  -- {
+  --   -- Plugin for the Everforest color scheme
+  --   "neanias/everforest-nvim",
+  --   version = false, -- Use the latest version
+  --   lazy = false, -- Load this plugin immediately
+  --   config = function()
+  --     require("everforest").setup({
+  --       background = "hard", -- Set the background to 'hard'
+  --       italis = true, -- Enable italics
+  --       transparent_background_level = 1, -- Uncomment to enable transparency
+  --       diagnostic_text_highlight = true, -- Enable diagnostic text highlight
+  --       diagnostic_virtual_text = "coloured", -- Set diagnostic virtual text to colored
+  --       colours_override = function(palette)
+  --         palette.bg0 = "#1A1A22" -- Override background color
+  --       end,
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   -- Plugin for the Oldworld color scheme
+  --   "dgox16/oldworld.nvim",
+  --   lazy = false, -- Load this plugin immediately
+  --   priority = 1000, -- High priority to ensure it loads early
+  -- },
   {
     -- LazyVim configuration
     "LazyVim/LazyVim",
