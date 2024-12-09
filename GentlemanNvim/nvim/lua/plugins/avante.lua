045709ddd88aceea345e76b59668b10d202888de
Alan-TheGentleman <alanbuscaglia@gmail.com>
2024-12-09 13:08:04 +0100

    feat: change ask floating window - avante

--- GentlemanNvim/nvim/lua/plugins/avante.lua
+++ GentlemanNvim/nvim/lua/plugins/avante.lua
@@ -68,7 +68,7 @@ return {
         start_insert = true, -- Start insert mode when opening the edit window
       },
       ask = {
-        floating = true, -- Open the 'AvanteAsk' prompt in a floating window
+        floating = false, -- Open the 'AvanteAsk' prompt in a floating window
         start_insert = true, -- Start insert mode when opening the ask window
         ---@type "ours" | "theirs"
         focus_on_apply = "ours", -- which diff to focus after applying
