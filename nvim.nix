c3c23d99edba00a747a5af61e5074bb253e36dd3
AlanBuscaglia <anua@gft.com>
2025-02-19 15:17:38 +0100

    fix: create .config if missing

--- nvim.nix
+++ nvim.nix
@@ -3,6 +3,12 @@
   home.activation.copyNvim = lib.hm.dag.entryAfter ["writeBoundary"] ''
     echo "Copying nvim configuration..."
     rm -rf "$HOME/.config/nvim"
+
+    # Verificar si el directorio .config existe, si no lo crea
+    if [ ! -d "$HOME/.config" ]; then
+      mkdir -p "$HOME/.config"
+    fi
+
     cp -r ${toString ./nvim} "$HOME/.config/nvim"
     chmod -R u+w "$HOME/.config/nvim"
   '';
