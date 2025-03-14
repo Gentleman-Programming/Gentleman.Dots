52ecfffb93644690ebfd4788249f64994adf15e8
AlanBuscaglia <anua@gft.com>
2025-03-14 11:21:18 +0100

    feat: using avante instead of copilot chat

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -8,11 +8,15 @@ return {
     "akinsho/bufferline.nvim",
     enabled = false, -- Disable this plugin
   },
+  -- {
+  --   -- Plugin para mejorar la experiencia de edición en Neovim
+  --   -- URL: https://github.com/yetone/avante.nvim
+  --   -- Description: Este plugin ofrece una serie de mejoras y herramientas para optimizar la edición de texto en Neovim.
+  --   "yetone/avante.nvim",
+  --   enabled = false,
+  -- },
   {
-    -- Plugin para mejorar la experiencia de edición en Neovim
-    -- URL: https://github.com/yetone/avante.nvim
-    -- Description: Este plugin ofrece una serie de mejoras y herramientas para optimizar la edición de texto en Neovim.
-    "yetone/avante.nvim",
+    "CopilotC-Nvim/CopilotChat.nvim",
     enabled = false,
   },
 }
