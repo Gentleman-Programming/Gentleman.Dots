f19d8929b7fc90bda7cfafd26551c5ce2de47759
Alan Buscaglia <alanbuscaglia@Alans-MacBook-Pro.local>
2025-04-14 18:03:13 +0200

    fix: ghostty and nvim theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,36 +1,46 @@
 return {
   {
+    -- {
+    --   "xiyaowong/transparent.nvim",
+    --   config = function()
+    --     require("transparent").setup({
+    --       extra_groups = { -- table/string: additional groups that should be cleared
+    --         "Normal",
+    --         "NormalNC",
+    --         "Comment",
+    --         "Constant",
+    --         "Special",
+    --         "Identifier",
+    --         "Statement",
+    --         "PreProc",
+    --         "Type",
+    --         "Underlined",
+    --         "Todo",
+    --         "String",
+    --         "Function",
+    --         "Conditional",
+    --         "Repeat",
+    --         "Operator",
+    --         "Structure",
+    --         "LineNr",
+    --         "NonText",
+    --         "SignColumn",
+    --         "CursorLineNr",
+    --         "EndOfBuffer",
+    --       },
+    --       exclude = {}, -- table: groups you don't want to clear
+    --     })
+    --   end,
+    -- },
     {
-      "xiyaowong/transparent.nvim",
-      config = function()
-        require("transparent").setup({
-          extra_groups = { -- table/string: additional groups that should be cleared
-            "Normal",
-            "NormalNC",
-            "Comment",
-            "Constant",
-            "Special",
-            "Identifier",
-            "Statement",
-            "PreProc",
-            "Type",
-            "Underlined",
-            "Todo",
-            "String",
-            "Function",
-            "Conditional",
-            "Repeat",
-            "Operator",
-            "Structure",
-            "LineNr",
-            "NonText",
-            "SignColumn",
-            "CursorLineNr",
-            "EndOfBuffer",
-          },
-          exclude = {}, -- table: groups you don't want to clear
-        })
-      end,
+      "catppuccin/nvim",
+      name = "catppuccin",
+      priority = 1000,
+      opts = {
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        transparent_background = true, -- disables setting the background color.
+        term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      },
     },
     {
       "Alan-TheGentleman/oldworld.nvim",
@@ -43,7 +53,7 @@ return {
       "LazyVim/LazyVim",
       opts = {
         -- Set the default color scheme
-        colorscheme = "oldworld",
+        colorscheme = "catppuccin",
       },
     },
   },
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -84,7 +84,7 @@ return {
     requires = { "nvim-tree/nvim-web-devicons", opt = true }, -- Optional dependency for icons
     opts = {
       options = {
-        theme = "oldworld", -- Set the theme for lualine
+        theme = "catppuccin", -- Set the theme for lualine
         icons_enabled = true, -- Enable icons in the statusline
       },
       sections = {
