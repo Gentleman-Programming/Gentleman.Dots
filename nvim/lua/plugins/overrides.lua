c9854258f1378e4bb7a4883c98c1f66faf09e02a
Alan Buscaglia <alanbuscaglia@Alans-MacBook-Pro.local>
2025-04-14 18:56:51 +0200

    fix: use nil from nix

--- nvim/lua/plugins/overrides.lua
+++ nvim/lua/plugins/overrides.lua
@@ -28,15 +28,26 @@ return {
     -- URL: https://github.com/neovim/nvim-lspconfig
     -- Description: Quickstart configurations for the Neovim LSP client.
     "neovim/nvim-lspconfig",
-    events = "VeryLazy", -- Load this plugin on the 'VeryLazy' event
+    event = "VeryLazy", -- Load this plugin on the 'VeryLazy' event
     opts = {
       inlay_hints = { enabled = false }, -- Disable inlay hints
       servers = {
         angularls = {
           -- Configuration for Angular Language Server
           root_dir = function(fname)
-            return require("lspconfig.util").root_pattern("angular.json", "project.json")(fname) -- Set root directory based on angular.json or project.json
+            return require("lspconfig.util").root_pattern("angular.json", "project.json")(fname)
           end,
+        },
+        nil_ls = {
+          -- Configuration for nil (Nix Language Server), already installed via nix
+          cmd = { "nil" },
+          autostart = true,
+          mason = false, -- Explicitly disable mason management for nil_ls
+          settings = {
+            ["nil"] = {
+              formatting = { command = { "nixpkgs-fmt" } },
+            },
+          },
         },
       },
     },
