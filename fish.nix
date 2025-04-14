701191bf2bd8277e7228174a4243f99127c30e94
Alan Buscaglia <alanbuscaglia@Alans-MacBook-Pro.local>
2025-04-14 18:44:24 +0200

    fix: fish nix config

--- fish.nix
+++ fish.nix
@@ -1,15 +1,65 @@
-{ pkgs, lib, ... }: 
+{ pkgs, lib, ... }:
 {
   programs.fish = {
     enable = true;
-    
+
     interactiveShellInit = ''
-      # Set Catppuccin Mocha as default theme on startup
       if status is-interactive
-        fish_config theme choose "Catppuccin Mocha"
+          # Install Fisher if not installed
+          if not functions -q fisher
+              curl -sL https://git.io/fisher | source
+              fisher install jorgebucaran/fisher
+          end
+
+          # Set Catppuccin Mocha as default theme
+          fish_config theme choose "Catppuccin Mocha"
       end
+
+      # Set BREW_BIN based on OS
+      if test (uname) = Darwin
+          set BREW_BIN /opt/homebrew/bin/brew
+      else
+          set BREW_BIN /home/linuxbrew/.linuxbrew/bin/brew
+      end
+
+      # Set PATH
+      set -x PATH $HOME/.volta/bin $HOME/.bun/bin $HOME/.nix-profile/bin /nix/var/nix/profiles/default/bin $PATH /usr/local/bin $HOME/.config $HOME/.cargo/bin /usr/local/lib/*
+
+      eval ($BREW_BIN shellenv)
+
+      starship init fish | source
+      zoxide init fish | source
+      atuin init fish | source
+      fzf --fish | source
+
+      set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense'
+
+      if not test -d ~/.config/fish/completions
+          mkdir -p ~/.config/fish/completions
+      end
+
+      if not test -f ~/.config/fish/completions/.initialized
+          carapace --list | awk '{print $1}' | xargs -I{} touch ~/.config/fish/completions/{}.fish
+          touch ~/.config/fish/completions/.initialized
+      end
+
+      carapace _carapace | source
+
+      set -x LS_COLORS "di=38;5;67:ow=48;5;60:ex=38;5;132:ln=38;5;144:*.tar=38;5;180:*.zip=38;5;180:*.jpg=38;5;175:*.png=38;5;175:*.mp3=38;5;175:*.wav=38;5;175:*.txt=38;5;223:*.sh=38;5;132"
+      set -g fish_greeting ""
+
+      if test (uname) = Darwin
+          alias ls='ls --color=auto'
+      else
+          alias ls='gls --color=auto'
+      end
+
+      alias fzfbat='fzf --preview="bat --theme=gruvbox-dark --color=always {}"'
+      alias fzfnvim='nvim (fzf --preview="bat --theme=gruvbox-dark --color=always {}")'
+
+      clear
     '';
-    
+
     plugins = [
       {
         name = "fisher";
@@ -31,17 +81,4 @@
       }
     ];
   };
-
-  home.activation.copyFish = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    echo "Copying fish configuration..."
-    rm -rf "$HOME/.config/fish"
-
-    cp -r ${toString ./fish} "$HOME/.config/fish"
-    chmod -R u+w "$HOME/.config/fish"
-    
-    # Ensure directories exist for Fisher
-    mkdir -p "$HOME/.config/fish/fisher/functions"
-    mkdir -p "$HOME/.config/fish/fisher/completions"
-    mkdir -p "$HOME/.config/fish/fisher/conf.d"
-  '';
 }
