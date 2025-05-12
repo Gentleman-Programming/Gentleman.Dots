bf2f6111f6d17db3151ea9bec37aba62f37acc99
Alan Buscaglia <alanbuscaglia@Alans-MacBook-Pro.local>
2025-05-12 21:28:02 +0200

    fix: kanagawa transparency
    fix: kanagawa linenr background none3

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -52,9 +52,34 @@ return {
       "rebelot/kanagawa.nvim",
       priority = 1000,
       lazy = true,
-      opts = {
-        theme = "wave",
-      },
+      config = function()
+        require("kanagawa").setup({
+          compile = false, -- enable compiling the colorscheme
+          undercurl = true, -- enable undercurls
+          commentStyle = { italic = true },
+          functionStyle = {},
+          keywordStyle = { italic = true },
+          statementStyle = { bold = true },
+          typeStyle = {},
+          transparent = true, -- do not set background color
+          dimInactive = true, -- dim inactive window `:h hl-NormalNC`
+          terminalColors = true, -- define vim.g.terminal_color_{0,17}
+          colors = { -- add/modify theme and palette colors
+            palette = {},
+            theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+          },
+          overrides = function(colors) -- add/modify highlights
+            return {
+              LineNr = { bg = "none" },
+            }
+          end,
+          theme = "wave", -- Load "wave" theme
+          background = { -- map the value of 'background' option to a theme
+            dark = "wave", -- try "dragon" !
+            light = "lotus",
+          },
+        })
+      end,
     },
     {
       -- LazyVim configuration
