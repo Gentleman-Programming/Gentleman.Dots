bf1ae6c4ddede524f6822204e7f8b6e6498b8933
Deuri Vasquez <deuriib@gmail.com>
2025-05-22 13:48:51 -0400

    Update mason.lua

--- GentlemanNvim/nvim/lua/plugins/mason.lua
+++ GentlemanNvim/nvim/lua/plugins/mason.lua
@@ -1,4 +1,11 @@
 return {
-  { "mason-org/mason.nvim", version = "1.11.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
+  { "mason-org/mason.nvim", version = "*" },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+  },
 }
