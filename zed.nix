6b74663b8cab969c939d8086c76aad6cb913bb92
Alan Buscaglia <alanbuscaglia@Alans-MacBook-Pro.local>
2025-05-24 11:01:26 +0200

    fix: zed now replaces the folder on install

--- zed.nix
+++ zed.nix
@@ -3,14 +3,8 @@
   home.activation.copyZed = lib.hm.dag.entryAfter ["writeBoundary"] ''
     echo "Copying Zed configuration..."
     rm -rf "$HOME/.config/zed"
-
-    # Check if the .config directory exists, if not, create it
-    if [ ! -d "$HOME/.config/zed" ]; then
-      mkdir -p "$HOME/.config/zed"
-    fi
-
+    mkdir -p "$HOME/.config/zed"
     cp -r ${toString ./zed}/* "$HOME/.config/zed/"
     chmod -R u+w "$HOME/.config/zed"
   '';
 }
-
