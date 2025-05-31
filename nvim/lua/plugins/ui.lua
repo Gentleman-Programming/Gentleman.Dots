6a1c8edd3386c69e4fa80407da553ea8f2327f94
Alan Buscaglia <alanbuscaglia@Alans-MacBook-Pro.local>
2025-06-01 01:21:33 +0200

    fix: add lualine modification

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -84,7 +84,7 @@ return {
     requires = { "nvim-tree/nvim-web-devicons", opt = true }, -- Optional dependency for icons
     opts = {
       options = {
-        theme = "kanagawa", -- Set the theme for lualine
+        theme = "gentleman-kanagawa-blur", -- Set the theme for lualine
         icons_enabled = true, -- Enable icons in the statusline
       },
       sections = {
