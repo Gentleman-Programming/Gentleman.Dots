bf80ab30f536ac9a95f0e2570242962a674b124f
Deuri Vasquez <deuriib@gmail.com>
2025-07-04 09:07:31 -0400

    feat: update plugin configurations and remove git branch from zellij layout

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,7 +1,7 @@
 return {
   "epwalsh/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
-  lazy = true,
+  lazy = false,
   ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
