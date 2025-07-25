b95d6ac1d0df9592b701194249cc96930b4cf0a9
Alan-TheGentleman <gentlemanprogramming@gmail.com>
2025-07-25 11:03:48 +0200

    fix: tmux fix

--- tmux.nix
+++ tmux.nix
@@ -71,6 +71,9 @@ bind K confirm-before -p "Kill all other sessions? (y/n)" "kill-session -a"
 set -g base-index 1
 setw -g pane-base-index 1
 
+# Fix opencode and gemini cli shift + enter
+set -g extended-keys always
+
 run '~/.tmux/plugins/tpm/tpm'
       '';
     };
