07ade021b7445f3f3d28ee1ddc860f487f136c6f
Deuri Vasquez <deuriib@gmail.com>
2025-10-04 06:24:40 -0400

    Create floaterm.lua

---
+++ nvim/lua/plugins/floaterm.lua
@@ -0,0 +1,75 @@
+return {
+  "voldikss/vim-floaterm",
+  event = "VeryLazy",
+  keys = {
+    -- Toggle terminal
+    -- { "<leader>ft", "<cmd>FloatermToggle<cr>", desc = "Toggle Terminal" },
+
+    -- New terminals
+    { "<M-j>", "<cmd>FloatermNew<cr>", mode = { "n", "t" }, desc = "New Terminal" },
+
+    -- Navigation
+    { "<M-h>", "<cmd>FloatermPrev<cr>", mode = "t", desc = "Prev Terminal" },
+    { "<M-l>", "<cmd>FloatermNext<cr>", mode = { "t" }, desc = "Next Terminal" },
+    {
+      "<M-k>",
+      "<cmd>FloatermKill<cr><cmd>FloatermPrev<cr>",
+      mode = "t",
+      desc = "Kill Term and Go to Previous",
+    },
+
+    -- Special terminals
+    { "<leader>fN", "<cmd>FloatermNew --wintype=normal --position=right<cr>", desc = "New Terminal (Right Split)" },
+
+    -- Terminal mode mappings
+    { "<Esc><Esc>", "<C-\\><C-n>", mode = "t", desc = "Switch to Normal mode" },
+    -- { "<C-_>", "<cmd>FloatermToggle<cr>", mode = "t", desc = "Hide Terminal" },
+    { "<M-f>", "<cmd>FloatermToggle<cr>", mode = { "t", "n" }, desc = "Toggle Terminal" },
+
+    -- Normal mode mappings in terminal
+    { "i", "i", mode = "n", buffer = true, desc = "Enter Insert mode" },
+    { "q", "<cmd>FloatermToggle<cr>", mode = "n", buffer = true, desc = "Quit Terminal" },
+  },
+  config = function()
+    -- Window appearance
+    vim.g.floaterm_width = 0.5
+    vim.g.floaterm_height = 0.5
+    vim.g.floaterm_title = "Terminal ($1/$2)"
+    vim.g.floaterm_position = "center"
+    --
+    -- Border settings (rounded corners)
+    vim.g.floaterm_borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+    vim.g.floaterm_borderhighlight = "FloatermBorder"
+    vim.g.floaterm_borderless = 0
+    --
+    -- Terminal behavior
+    vim.g.floaterm_autoclose = 1
+    vim.g.floaterm_autohide = 1 -- Hide instead of close when using Esc
+    --
+    -- Scrollback limit (critical for performance)
+    vim.g.floaterm_scrollback = 1000 -- Lines to keep in buffer (default 10000)
+    --
+    -- Shell configuration
+    vim.g.floaterm_shell = vim.o.shell -- Use your default shell
+    vim.g.floaterm_rootmarkers = { ".git", "package.json" } -- Auto-detect project root
+    --
+    -- Keymaps for terminal job control
+    vim.g.floaterm_keymap_toggle = "<leader>ft"
+    vim.g.floaterm_keymap_kill = "<leader>fk"
+    --
+    -- Highlight groups
+    vim.api.nvim_set_hl(0, "FloatermBorder", { link = "FloatBorder" })
+    vim.api.nvim_set_hl(0, "Floaterm", { bg = nil }) -- Custom background
+
+    -- Auto-commands
+    vim.api.nvim_create_autocmd("TermOpen", {
+      pattern = "floaterm",
+      callback = function()
+        vim.opt_local.number = false
+        vim.opt_local.relativenumber = false
+        vim.opt_local.signcolumn = "no"
+        vim.cmd("startinsert")
+      end,
+    })
+  end,
+}
