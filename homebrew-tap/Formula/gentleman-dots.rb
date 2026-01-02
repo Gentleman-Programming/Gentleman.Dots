fb7977c3c7558a4a98ffe9ad7096ae022856740b
Alan Buscaglia <gentlemanprogramming@gmail.com>
2026-01-02 15:41:28 +0100

    chore(brew): update formula to v2.4.3

--- homebrew-tap/Formula/gentleman-dots.rb
+++ homebrew-tap/Formula/gentleman-dots.rb
@@ -1,36 +1,38 @@
 class GentlemanDots < Formula
   desc "Interactive TUI installer for Gentleman.Dots development environment"
   homepage "https://github.com/Gentleman-Programming/Gentleman.Dots"
-  version "1.0.0"
+  version "2.4.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Gentleman-Programming/Gentleman.Dots/releases/download/v#{version}/gentleman-dots-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_ARM64"
+      url "https://github.com/Gentleman-Programming/Gentleman.Dots/releases/download/v#{version}/gentleman-installer-darwin-arm64"
+      sha256 "67f9b71d67f6f16539563693bf5b94349dc4bb36373ed582e97ecbd16d5b991f"
     end
     on_intel do
-      url "https://github.com/Gentleman-Programming/Gentleman.Dots/releases/download/v#{version}/gentleman-dots-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_AMD64"
+      url "https://github.com/Gentleman-Programming/Gentleman.Dots/releases/download/v#{version}/gentleman-installer-darwin-amd64"
+      sha256 "e89af35d7fc9e3b6484ea34d1f5d25fffc1f71d5bfed6b7b45dca7028bef1733"
     end
   end
 
   on_linux do
-    on_arm do
-      url "https://github.com/Gentleman-Programming/Gentleman.Dots/releases/download/v#{version}/gentleman-dots-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
-    end
     on_intel do
-      url "https://github.com/Gentleman-Programming/Gentleman.Dots/releases/download/v#{version}/gentleman-dots-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_AMD64"
+      url "https://github.com/Gentleman-Programming/Gentleman.Dots/releases/download/v#{version}/gentleman-installer-linux-amd64"
+      sha256 "97c0468fb846a9d66988609ab40e4517697c4819311f76582c97480a2429575a"
     end
   end
 
   def install
-    bin.install "gentleman-dots"
+    if OS.mac? && Hardware::CPU.arm?
+      bin.install "gentleman-installer-darwin-arm64" => "gentleman-dots"
+    elsif OS.mac? && Hardware::CPU.intel?
+      bin.install "gentleman-installer-darwin-amd64" => "gentleman-dots"
+    elsif OS.linux? && Hardware::CPU.intel?
+      bin.install "gentleman-installer-linux-amd64" => "gentleman-dots"
+    end
   end
 
   test do
-    assert_match "Gentleman.Dots Installer", shell_output("#{bin}/gentleman-dots --version")
+    system "#{bin}/gentleman-dots", "--help"
   end
 end
