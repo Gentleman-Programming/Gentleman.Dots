2080b0e183faf148378a950dd9c2ff9e7e70bc85
Alan Buscaglia <gentlemanprogramming@gmail.com>
2026-01-10 20:55:30 +0100

    chore(homebrew): bump version to v2.7.1

--- homebrew-tap/Formula/gentleman-dots.rb
+++ homebrew-tap/Formula/gentleman-dots.rb
@@ -1,28 +1,28 @@
 class GentlemanDots < Formula
   desc "Interactive TUI installer for Gentleman.Dots development environment"
   homepage "https://github.com/Gentleman-Programming/Gentleman.Dots"
-  version "2.6.2"
+  version "2.7.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/Gentleman-Programming/Gentleman.Dots/releases/download/v#{version}/gentleman-installer-darwin-arm64"
-      sha256 "658b7d2fce3543810c1a992ee80f1d10ff95c76fc3eb425585c1e5911569a794"
+      sha256 "bf36cf774d18c522cb1888df702a6ed29c9b59cc4cdf7b1af3795c1c6266bf3c"
     end
     on_intel do
       url "https://github.com/Gentleman-Programming/Gentleman.Dots/releases/download/v#{version}/gentleman-installer-darwin-amd64"
-      sha256 "ff215e4302c06c7351fccbdbb18da3de2a3558b59241611ec4251c0d5c52bd94"
+      sha256 "d279e84f43a455692f5aa5ef8fb6e6c976437c481b9423831e5d90733167635c"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/Gentleman-Programming/Gentleman.Dots/releases/download/v#{version}/gentleman-installer-linux-arm64"
-      sha256 "8e753830d22fbf7f9ed9c2bdab275fa7a25b788bb087749ac15874c6c3ac3b78"
+      sha256 "d6b7c20bf9e95829c760d53c2d44db49dfe0e43d009dbab7cbb991d6d75d6296"
     end
     on_intel do
       url "https://github.com/Gentleman-Programming/Gentleman.Dots/releases/download/v#{version}/gentleman-installer-linux-amd64"
-      sha256 "fe31a69b463cee15f88443fcbb8a5eb88f302cb2f906e69d23236c277c6db548"
+      sha256 "958b49abf5ee7488f70db55da23cdcaf8a330d369ec85635e342af7990a97bda"
     end
   end
 
