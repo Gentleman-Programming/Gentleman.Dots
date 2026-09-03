6f44b797b016aea92772d8a6d81a5f1bc53a84bb
Alan Buscaglia <gentlemanprogramming@gmail.com>
2026-09-03 11:40:51 +0200

    fix(nvim): sync clipboard over SSH

--- GentlemanNvim/nvim/lua/config/options.lua
+++ GentlemanNvim/nvim/lua/config/options.lua
@@ -1,3 +1,21 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.clipboard = "unnamedplus"
+
+if vim.env.SSH_CONNECTION then
+  local osc52 = require("vim.ui.clipboard.osc52")
+
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = osc52.copy("+"),
+      ["*"] = osc52.copy("*"),
+    },
+    paste = {
+      ["+"] = osc52.paste("+"),
+      ["*"] = osc52.paste("*"),
+    },
+  }
+end
